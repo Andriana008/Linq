10cf41a626627ca0c9828f21b55d38f57b7214a4
Andriana008 <andriana080999@gmail.com>
2018-05-20 15:29:02 +0300

    Linq

---
+++ Client.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Linq
+{
+    public partial class Program
+    {
+        static StreamWriter wr = new StreamWriter("/Users/Andriana/Desktop/C#/practice/Linq/Linq/ClassRes.txt");
+
+        public class Client
+        {
+            private string id;
+            private string year;
+            private string mounth;
+            private string occupation;
+
+            public Client(string d = " ", string y = " ", string m = " ", string o = " ")
+            {
+                this.id = d;
+                this.year = y;
+                this.mounth = m;
+                this.occupation = o;
+            }
+
+            public string Id { get => id; set => id = value; }
+            public string Year { get => year; set => year = value; }
+            public string Mounth { get => mounth; set => mounth = value; }
+            public string Occupation { get => occupation; set => occupation = value; }
+
+        }
+        public static Client ReadFromFile(StreamReader streamReader)
+        {
+            Client c = new Client
+            {
+                Id = streamReader.ReadLine(),
+                Year = streamReader.ReadLine(),
+                Mounth = streamReader.ReadLine(),
+                Occupation = streamReader.ReadLine()
+            };
+            return c;
+        }
+        public static List<Client> ReadClassFromFile()
+        {
+            List<Client> clients = new List<Client>();
+            StreamReader streamReader = new StreamReader("/Users/Andriana/Desktop/C#/practice/Linq/Linq/Class.txt");
+            int ab = Convert.ToInt32(streamReader.ReadLine());
+            for (int i = 0; i < ab; i++)
+            {
+                clients.Add(ReadFromFile(streamReader));
+            }
+            return clients;
+        }
+        public static void Tasks(List<Client> clients)
+        {
+            var min_ocup = clients.Min(i => i.Occupation);
+            wr.WriteLine("Minimal occupation in hours:");
+            wr.WriteLine(min_ocup);
+            var max_ocup = clients.Max(i => i.Occupation);
+            wr.WriteLine("Maximal occupation in hours:");
+            wr.WriteLine(max_ocup);
+            wr.WriteLine("Maximal occupation year:");
+            IEnumerable<IGrouping<string, Client>> groupedByYears = clients.GroupBy(x => x.Year);
+            IEnumerable<IGrouping<string, Client>> sorted = groupedByYears.OrderByDescending(group => group.Sum(i => Convert.ToInt32(i.Occupation))).ThenBy(x => x.Min(y => y.Year));
+            wr.WriteLine(sorted.First().Key);
+            wr.WriteLine("Sum of occupations for all years:");
+            IEnumerable<IGrouping<string, Client>> groupedByCode = clients.GroupBy(x => x.Id);
+            SortedDictionary<string, string> durations = new SortedDictionary<string, string>();
+            foreach (IGrouping<string, Client> group in groupedByCode)
+            {
+                int sum = group.Sum(x => Convert.ToInt32(x.Occupation));
+                durations.Add(Convert.ToString(sum), group.Key);
+            }
+            foreach (KeyValuePair<string, string> group in durations)
+            {
+                wr.WriteLine("Occpation : {0} ,Id: {1}", group.Key, group.Value);
+            }
+            wr.WriteLine("Count of mount");
+            IEnumerable<IGrouping<string, Client>> groupedByCode2 = clients.GroupBy(x => x.Id);
+            SortedDictionary<int, int> months = new SortedDictionary<int, int>();
+            foreach (IGrouping<string, Client> group in groupedByCode2)
+            {
+                int count = group.Count();
+                months.Add(Convert.ToInt32(group.Key), count);
+            }
+            foreach (KeyValuePair<int, int> group in months)
+            {
+                wr.WriteLine("Id : {0} ,Occpation: {1}", group.Key, group.Value);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Linq
+{
+    public partial class Program
+    {
+        static StreamWriter writer = new StreamWriter("/Users/Andriana/Desktop/C#/practice/Linq/Linq/res.txt");
+
+        public static void LinqBegin16()
+        {
+            string[] arr = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task16-18.txt");
+            IEnumerable<string> res = from number in arr where Int32.Parse(number) > 0 select number;
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            writer.WriteLine("Task 16");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }
+            writer.WriteLine();
+        }
+        public static void LinqBegin17()
+        {
+            string[] arr = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task16-18.txt");
+            IEnumerable<string> res = (from number in arr where Convert.ToInt32(number) % 2 == 1 select number).Distinct();
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            writer.WriteLine("Task 17");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }
+            writer.WriteLine();
+
+        }
+        public static void LinqBegin18()
+        {
+            string[] arr = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task16-18.txt");
+            IEnumerable<string> res = from number in arr where (Convert.ToInt32(number) > 0) && (Convert.ToInt32(number) - 10 > 0) && (Convert.ToInt32(number) - 100 < 0) select number;
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            writer.WriteLine("Task 18");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }
+            writer.WriteLine();
+
+        }
+        public static void LinqBegin19()
+        {
+            string[] arr = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task19.txt");
+            IEnumerable<string> res= arr.OrderBy(i => i.Length).ThenBy(i => i);
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+            writer.WriteLine("Task 19");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }        
+        }
+        public static void LinqBegin20()
+        {
+            string[] arr = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task16-18.txt");
+            int d = 57;
+            IEnumerable<string> res = arr.SkipWhile(num => Convert.ToInt32(num) < d).Where(num => Convert.ToInt32(num) % 2 != 0 && Convert.ToInt32(num) > 0).Reverse();
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+
+            writer.WriteLine("Task 20");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }
+            writer.WriteLine();
+
+        }
+        public static void LinqBegin44()
+        {
+            string[] arr1 = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task16-18.txt");
+            string[] arr2 = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task44.txt");
+            int k1 = 36;
+            int k2 = 67;
+            IEnumerable<string> res = (from i in arr1 where Convert.ToInt32(i) > k1 select i).Union(from i in arr2 where Convert.ToInt32(i) < k2 select i).OrderBy(i => i);
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+            writer.WriteLine("Task 44");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }
+            writer.WriteLine();
+        }
+        public static void LinqBegin45()
+        {
+            string[] arr1 = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task19.txt");
+            string[] arr2 = File.ReadAllLines("/Users/Andriana/Desktop/C#/practice/Linq/Linq/task45.txt");
+            int l1 = 3;
+            int l2 = 4;
+            IEnumerable<string> res = (from i in arr1 where i.Length == l1 select i).Union(from i in arr2 where i.Length == l2 select i).OrderBy(i => i);
+            foreach (string i in res)
+            {
+                Console.WriteLine(i);
+            }
+            writer.WriteLine("Task 45");
+            foreach (var item in res)
+            {
+                writer.Write($"{item} ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            LinqBegin16();
+            LinqBegin17();
+            LinqBegin18();
+            LinqBegin19();
+            LinqBegin20();
+            LinqBegin44();
+            LinqBegin45();
+            List<Client> c = ReadClassFromFile();
+            Tasks(c);
+            writer.Close();
+            wr.Close();
+        }
+    }
+}
